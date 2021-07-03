ffb46492b43ffbc93ea32f7023102c10bc4e181c
Isabela Santos <81272922+Isabela-prog@users.noreply.github.com>
2021-07-03 18:29:22 -0300

    Update parquimetro.py

--- SEMANA01/parquimetro.py
+++ SEMANA01/parquimetro.py
@@ -1,3 +1,27 @@
+"""
+Descri¸cão do problema:
+Você foi contratado para montar um sistema de parquímetro, que calcula a taxa cobrada por estacionamento
+de veículos em um via da cidade. O sistema recebe os horários de entrada e saída do veículo, e deve calcular o
+valor a ser pago usando o seguinte critério:
+
+• 1 hora: R$ 5,00
+• De 2 a 4 horas: R$ 3,00 por hora
+• 5 horas ou mais: R$ 2,00 por hora
+• Tolerância de 15min. a partir da chegada.
+
+O valor a ser pago sempre considera a hora cheia, ou seja, se o veículo ficar estacionado durante 1,5 horas, será
+cobrado o valor equivalente a 2 horas.
+O parquímetro registra os horários de chegada e saída do veículo, no formato HH:MM. Considere, por simplicidade, que o parquímetro só registra veículos estacionados no mesmo dia. Dessa forma, o horário de saída
+sempre é superior ao horário de chegada e correspondem ao mesmo dia. O horário pode variar de 00:00 a 23:59.
+
+Entrada:
+Seu programa receberá, como entrada, duas linhas de valores. Cada uma delas possui um horário no formato
+HH:MM. A primeira linha refere-se ao horário de chegada do veículo e a segunda linha refere-se ao horário de
+saída do mesmo.
+Saída:
+A saída exibirá o valor de deve ser cobrado como taxa de estacionamento, em Reais (R$).
+"""
+
 import math
 
 entrada = input()
